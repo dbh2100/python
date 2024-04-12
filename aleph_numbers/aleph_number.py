c3a027d96e3fd3aebb8fd553f998941eba84cc1d
David Harrison <166529768+dbh2100@users.noreply.github.com>
2024-04-12 17:22:08 -0400

    Cleaning up code

--- aleph_numbers/aleph_number.py
+++ aleph_numbers/aleph_number.py
@@ -1,3 +1,5 @@
+"""This module defines an aleph number Python class"""
+
 from numbers import Integral, Real
 
 INFINITY = float('inf')
@@ -8,21 +10,22 @@ class AlephNumber(Integral):
     aleph_0 ("aleph naught") is the cardinality of the set of positive integers.
     aleph_n+1 is the number of combinations of aleph_n items.
     '''
-    
+
     def __init__(self, n=0):
         if not isinstance(n, int):
             raise TypeError('n must be an int')
         if n < 0:
             raise ValueError('n must be equal to or greater than 0')
         self._n = n
-        
+
     @property
     def n(self):
+        """Get the aleph number's subscript"""
         return self._n
-        
+
     def __repr__(self):
-        return 'aleph_%d' % self.n
-        
+        return f'aleph_{self.n}'
+
     def __lshift__(self, other):
         return NotImplemented
 
@@ -32,39 +35,44 @@ class AlephNumber(Integral):
     def __rand__(self, other):
         return NotImplemented
 
-    def __pow__(self, other):
-        if isinstance(other, (int, float)):
-            if other == INFINITY:
+    def __pow__(self, exponent, modulus=None):
+
+        if isinstance(exponent, (int, float)):
+
+            if exponent == INFINITY:
                 return NotImplemented
-            elif other > 0:
+
+            if exponent > 0:
                 return AlephNumber(self.n)
-            elif other == 0:
+
+            if exponent == 0:
                 return 1
-            else:
-                return 0
-        else:
-            return NotImplemented
+
+            return 0
+
+        return NotImplemented
 
     def __rtruediv__(self, other):
+
         if isinstance(other, AlephNumber):
             if self.n < other.n:
                 return AlephNumber(other.n)
-            elif self.n == other.n:
+            if self.n == other.n:
                 return 1
-            elif self.n > other.n:
+            if self.n > other.n:
                 return 0
-        elif isinstance(other, Real) and other < INFINITY:
+
+        if isinstance(other, Real) and other < INFINITY:
             return 0
-        else:
-            return NotImplemented
+
+        return NotImplemented
 
     def __radd__(self, other):
         if isinstance(other, AlephNumber):
             return AlephNumber(max(self.n, other.n))
-        elif isinstance(other, Real) and other < INFINITY:
+        if isinstance(other, Real) and other < INFINITY:
             return AlephNumber(self.n)
-        else:
-            return NotImplemented
+        return NotImplemented
 
     def __xor__(self, other):
         return NotImplemented
@@ -73,9 +81,8 @@ class AlephNumber(Integral):
         return NotImplemented
 
     def __eq__(self, other):
-        if isinstance(other, AlephNumber):
-            if self.n == other.n:
-                return True
+        if isinstance(other, AlephNumber) and self.n == other.n:
+            return True
         return False
 
     def __trunc__(self):
@@ -84,18 +91,16 @@ class AlephNumber(Integral):
     def __add__(self, other):
         if isinstance(other, AlephNumber):
             return AlephNumber(max(self.n, other.n))
-        elif isinstance(other, (int, float)) and other < INFINITY:
+        if isinstance(other, (int, float)) and other < INFINITY:
             return AlephNumber(self.n)
-        else:
-            return NotImplemented
+        return NotImplemented
 
     def __mul__(self, other):
         if isinstance(other, AlephNumber):
             return AlephNumber(max(self.n, other.n))
-        elif isinstance(other, (int, float)) and other < INFINITY:
+        if isinstance(other, (int, float)) and other < INFINITY:
             return AlephNumber(self.n)
-        else:
-            return NotImplemented
+        return NotImplemented
 
     def __neg__(self):
         return NotImplemented
@@ -103,42 +108,50 @@ class AlephNumber(Integral):
     def __invert__(self):
         return NotImplemented
 
-    def __rpow__(self, other):
-        if isinstance(other, Real):
-            if other == INFINITY:
+    def __rpow__(self, base):
+
+        if isinstance(base, Real):
+
+            if base == INFINITY:
                 return NotImplemented
-            elif other > 1:
+
+            if base > 1:
                 return AlephNumber(self.n + 1)
-            elif other == 1:
+
+            if base == 1:
                 return 1
-            elif other >= 0:
+
+            if base >= 0:
                 return 0
-            else:
-                return NotImplemented
-        else:
-            return NotImplemented
+
+        return NotImplemented
 
     def __rmul__(self, other):
+
         if isinstance(other, AlephNumber):
             return AlephNumber(max(self.n, other.n))
-        elif isinstance(other, Real) and other < INFINITY:
+
+        if isinstance(other, Real) and other < INFINITY:
             return AlephNumber(self.n)
-        else:
-            return NotImplemented
+
+        return NotImplemented
 
     def __floordiv__(self, other):
         return self / other
 
     def __truediv__(self, other):
+
         if isinstance(other, AlephNumber):
             if self.n < other.n:
                 return 0
-            elif self.n == other.n:
+            if self.n == other.n:
                 return 1
-            elif self.n > other.n:
+            if self.n > other.n:
                 return AlephNumber(self.n)
+
         if isinstance(other, (int, float)):
             return AlephNumber(self.n)
+
         return NotImplemented
 
     def __int__(self):
@@ -153,10 +166,9 @@ class AlephNumber(Integral):
     def __le__(self, other):
         if isinstance(other, AlephNumber):
             return self.n <= other.n
-        elif isinstance(other, Real) and other < INFINITY:
+        if isinstance(other, Real) and other < INFINITY:
             return False
-        else:
-            return NotImplemented
+        return NotImplemented
 
     def __rxor__(self, other):
         return NotImplemented
@@ -164,7 +176,7 @@ class AlephNumber(Integral):
     def __rfloordiv__(self, other):
         return other / self
 
-    def __round__(self):
+    def __round__(self, ndigits=None):
         return NotImplemented
 
     def __rshift__(self, other):
@@ -188,10 +200,9 @@ class AlephNumber(Integral):
     def __lt__(self, other):
         if isinstance(other, AlephNumber):
             return self.n < other.n
-        elif isinstance(other, Real) and other < INFINITY:
+        if isinstance(other, Real) and other < INFINITY:
             return False
-        else:
-            return NotImplemented
+        return NotImplemented
 
     def __abs__(self):
         return NotImplemented
@@ -204,6 +215,6 @@ class AlephNumber(Integral):
 
     def __ge__(self, other):
         return not self < other
-        
+
     def __float__(self):
         return INFINITY
--- aleph_numbers/aleph_number_test.py
+++ aleph_numbers/aleph_number_test.py
@@ -1,79 +1,82 @@
+"""Defines unittests for AlephNumber class"""
+
 from aleph_number import AlephNumber, INFINITY
 import unittest
 
 class TestAlephNumber(unittest.TestCase):
-    
+    """Unittest module for AlephNumber class"""
+
     def test_addition(self):
-        
+
         #Addition between aleph number and int
         self.assertEqual(AlephNumber(4) + 3, AlephNumber(4))
         self.assertEqual(-9 + AlephNumber(4), AlephNumber(4))
-        
+
         #Addition between aleph number and float
         self.assertEqual(AlephNumber(4) + -7.2, AlephNumber(4))
         self.assertEqual(3.5 + AlephNumber(4), AlephNumber(4))
-        
+
         #Addition between two aleph numbers
         self.assertEqual(AlephNumber(4) + AlephNumber(1), AlephNumber(4))
         self.assertEqual(AlephNumber(4) + AlephNumber(6), AlephNumber(6))
-        
+
         #Addition between aleph number and string, None, etc. not implemented
         for x in ('a', None, [3], (9,), INFINITY):
             with self.assertRaises(TypeError):
-                AlephNumber(4) + x
+                y = AlephNumber(4) + x
             with self.assertRaises(TypeError):
-                x + AlephNumber(4)
-                
+                y = x + AlephNumber(4)
+
     def test_subtraction(self):
-        
+
         #Subtraction between aleph number and int
         self.assertEqual(AlephNumber(4) - 3, AlephNumber(4))
-        
+
         #Subtraction between aleph number and float
         self.assertEqual(AlephNumber(4) - -7.2, AlephNumber(4))
-        
+
         #Subtraction between aleph number and string, None, other aleph number, etc. not implemented
         for x in ('a', None, [3], (9,), AlephNumber(1), AlephNumber(7)):
             with self.assertRaises(TypeError):
-                AlephNumber(4) - x
+                y = AlephNumber(4) - x
             with self.assertRaises(TypeError):
-                x - AlephNumber(4)
-                
+                y = x - AlephNumber(4)
+
     def test_multiplication(self):
-        
+
         #Multiplication between aleph number and int
         self.assertEqual(AlephNumber(4) * 3, AlephNumber(4))
         self.assertEqual(-9 * AlephNumber(4), AlephNumber(4))
-        
+
         #Multiplication between aleph number and float
         self.assertEqual(AlephNumber(4) * -7.2, AlephNumber(4))
         self.assertEqual(3.5 * AlephNumber(4), AlephNumber(4))
-        
+
         #Multiplication between two aleph numbers
         self.assertEqual(AlephNumber(4) * AlephNumber(1), AlephNumber(4))
         self.assertEqual(AlephNumber(4) * AlephNumber(6), AlephNumber(6))
-        
+
         #Multiplication between aleph number and string, None, etc. not implemented
         for x in ('a', None, [3], (9,), INFINITY):
             with self.assertRaises((TypeError, OverflowError)):
-                AlephNumber(4) * x
+                y = AlephNumber(4) * x
             with self.assertRaises((TypeError, OverflowError)):
-                x * AlephNumber(4)
-                
+                y = x * AlephNumber(4)
+
     def test_division(self):
-    
+
         #Division between aleph number and int
         self.assertEqual(AlephNumber(4) / 3, AlephNumber(4))
         self.assertEqual(-9 / AlephNumber(4), 0)
         self.assertEqual(AlephNumber(4) // 3, AlephNumber(4))
         self.assertEqual(-9 // AlephNumber(4), 0)
-        
+
         #Division between aleph number and float
         self.assertEqual(AlephNumber(4) / -7.2, AlephNumber(4))
         self.assertEqual(3.5 / AlephNumber(4), 0)
         self.assertEqual(AlephNumber(4) // -7.2, AlephNumber(4))
         self.assertEqual(3.5 // AlephNumber(4), 0)
-        
+
         #Division between two aleph numbers
         self.assertEqual(AlephNumber(4) / AlephNumber(1), AlephNumber(4))
         self.assertEqual(AlephNumber(4) / AlephNumber(4), 1)
@@ -81,26 +84,26 @@ class TestAlephNumber(unittest.TestCase):
         self.assertEqual(AlephNumber(4) // AlephNumber(1), AlephNumber(4))
         self.assertEqual(AlephNumber(4) // AlephNumber(4), 1)
         self.assertEqual(AlephNumber(4) // AlephNumber(6), 0)
-                
+
     def test_power(self):
         self.assertEqual(2 ** AlephNumber(4), AlephNumber(5))
-        
+
     def test_comparison(self):
-    
+
         #Aleph number should always be greater than finite int
         self.assertNotEqual(AlephNumber(4), 5000)
         self.assertLess(5000, AlephNumber(4))
         self.assertGreater(AlephNumber(4), 5000)
         self.assertLessEqual(5000, AlephNumber(4))
         self.assertGreaterEqual(AlephNumber(4), 5000)
-        
+
         #Aleph number should always be greater than finite float
         self.assertNotEqual(AlephNumber(4), 5000.2)
         self.assertLess(5000.2, AlephNumber(4))
         self.assertGreater(AlephNumber(4), 5000.2)
         self.assertLessEqual(5000.2, AlephNumber(4))
         self.assertGreaterEqual(AlephNumber(4), 5000.2)
-        
+
         #Comparison between aleph number and float infinity
         self.assertNotEqual(AlephNumber(4), INFINITY)
         with self.assertRaises(TypeError):
@@ -111,7 +114,7 @@ class TestAlephNumber(unittest.TestCase):
             AlephNumber(4) > INFINITY
         with self.assertRaises(TypeError):
             AlephNumber(4) >= INFINITY
-        
+
         #Comparison between aleph numbers
         self.assertEqual(AlephNumber(4), AlephNumber(4))
         self.assertNotEqual(AlephNumber(4), AlephNumber(6))
@@ -119,11 +122,12 @@ class TestAlephNumber(unittest.TestCase):
         self.assertTrue(AlephNumber(9) > AlephNumber(4) > AlephNumber(2))
         self.assertTrue(AlephNumber(2) <= AlephNumber(4) <= AlephNumber(4) <= AlephNumber(9))
         self.assertTrue(AlephNumber(9) >= AlephNumber(4) >= AlephNumber(4) >= AlephNumber(2))
-        
+
     def test_conversion(self):
         with self.assertRaises(OverflowError):
             int(AlephNumber(4))
         self.assertEqual(float(AlephNumber(4)), INFINITY)
-        
+
+
 if __name__ == '__main__':
     unittest.main()
