bbe26ce278e86d508b824e6a0def7117b9fc85b6
David Harrison <166529768+dbh2100@users.noreply.github.com>
2024-04-15 18:10:46 -0400

    Adding function which uses regular expressions

--- evaluate.py
+++ evaluate.py
@@ -1,13 +1,66 @@
 """This module defines a function that replicates Python's builtin eval()"""
 
+import re
+
+# Regular expression patterns
+PARENTHESIS_RE = re.compile(r'\( [^()]* \)', flags=re.VERBOSE)
+MULT_DIV_RE = re.compile(r'(-?\d+) \s* ([*/]) \s* (-?\d+)', flags=re.VERBOSE)
+ADD_SUB_RE = re.compile(r'(-?\d+) \s* ([+-]) \s* (-?\d+)', flags=re.VERBOSE)
+
+
+def _evaluate_binary(match_obj):
+
+    num1 = int(match_obj.group(1))
+    op = match_obj.group(2)
+    num2 = int(match_obj.group(3))
+
+    if op == '+':
+        return str(num1 + num2)
+
+    if op == '-':
+        return str(num1 - num2)
+
+    if op == '*':
+        return str(num1 * num2)
+
+    if op == '/':
+        return str(num1 / num2)
+
+    raise ValueError('Invalid operator')
+
+
+def evaluate_equation_regex(equation):
+    """Evaluate an equation of integers using regular expressions"""
+
+    # If used recursively, argument would be regex match object
+    # so convert to string and remove leading and trailing parentheses
+    return_string = False
+    if isinstance(equation, re.Match):
+        equation = equation.group(0).removeprefix('(').removesuffix(')')
+        return_string = True
+
+    # Evaluate all parenthetical components
+    while PARENTHESIS_RE.search(equation):
+        equation = PARENTHESIS_RE.sub(evaluate_equation_regex, equation)
+
+    # Multiplication and divison have order precedence
+    while MULT_DIV_RE.search(equation):
+        equation = MULT_DIV_RE.sub(_evaluate_binary, equation)
+
+    # Now execute addition and subtraction
+    while ADD_SUB_RE.search(equation):
+        equation = ADD_SUB_RE.sub(_evaluate_binary, equation)
+
+    return equation if return_string else int(equation)
+
+
 def evaluate_equation(equation):
-    """Evaluate an equation of integers"""
+    """Evaluate an equation of integers just using builtins"""
 
     if equation.isnumeric():
         return int(equation)
 
-    # Equation will be evaluated from right to left
-    # First remove leading "(" and trailing ")"
+    # First remove leading and trailing parentheses
     rev = reversed(equation.rstrip())
 
     # The transition will be the operator (+, -, *,  or /)
@@ -92,8 +145,12 @@ if __name__ == '__main__':
     for eq in test_equations:
         try:
             assert evaluate_equation(eq) == eval(eq)
+            assert evaluate_equation_regex(eq) == eval(eq)
         except AssertionError:
             print(f'Incorrect return for equation: {eq}')
-            print(f'Got: {evaluate_equation(eq)}')
+            print(f'evaluate_equation: {evaluate_equation(eq)}')
+            print(f'evaluate_equation_regex: {evaluate_equation_regex(eq)}')
             print(f'Expected: {eval(eq)}')
-            break
+            raise
+
+    print('All tests pass')
