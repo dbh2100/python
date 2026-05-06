b4d13263525f2780acd16fd9bcdd2366e9c21023
dbh2100 <davidbharrison@gmail.com>
2026-05-06 15:11:26 +0200

    Annotating instance_class_method.py

--- instance_class_method.py
+++ instance_class_method.py
@@ -1,23 +1,29 @@
 '''Defines a decorator class for a method that uses both the class and instance as variables'''
 
 import functools
+from   collections.abc import Callable
+from   typing import Any, TypeVar, Optional
+
+
+T = TypeVar('T')
+
 
 class InstanceClassMethod:
     '''Decorator class for a method
     To use the instance and class as the first and second argument in its signature'''
 
-    def __init__(self, func):
+    def __init__(self, func: Callable[..., Any]) -> None:
         '''Initializer, func is the function to be wrapped'''
         self.func = func
 
-    def __get__(self, obj, obj_type=None):
+    def __get__(self, obj: T, obj_type: Optional[type[T]]=None) -> Callable[..., Any]:
         '''obj is the instance and obj_type is the class'''
 
         if obj_type is None:
             obj_type = type(obj)
 
         @functools.wraps(self.func)
-        def new_func(*args, **kwargs):
+        def new_func(*args: Any, **kwargs: Any) -> Any:
             '''Replacement function'''
             return self.func(obj, obj_type, *args, **kwargs)
 
